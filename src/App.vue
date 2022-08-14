0fee3e330453866935276435f265c3f1e39ab766
Alberto Santiago <alberto92santiago@gmail.com>
2022-08-13 20:38:37 -0700

    Initial Config

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://www.facebook.com/impulsocolectivoensenada/" target="_blank">
+      <img src="./assets/banner.png" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Impulso Colectivo" />
 </template>
 
 <style scoped>
